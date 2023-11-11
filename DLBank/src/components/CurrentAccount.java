d1c230b7871e446925943a9aa9a69e1c9d69594d
davidllona <dllonaProgramador@gmail.com>
2023-11-11 04:27:06 +0100

    Creation of the CurrentAccount and SavingsAccount

---
+++ DLBank/src/components/CurrentAccount.java
@@ -0,0 +1,9 @@
+package components;
+
+public class CurrentAccount extends Account {
+    // Constructor calling the superclass constructor
+    public CurrentAccount(String label, Client client) {
+        super(label, client);
+    }
+}
+
---
+++ DLBank/src/components/SavingsAccount.java
@@ -0,0 +1,8 @@
+package components;
+
+public class SavingsAccount extends Account {
+    // Constructor calling the superclass constructor
+    public SavingsAccount(String label, Client client) {
+        super(label, client);
+    }
+}
